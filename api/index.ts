4a16a0e22026e9ae91494add167be1d50dd7e878
DobDoiDongDang <66070225@kmitl.ac.th>
2025-08-29 11:45:19 +0700

    ;-;

--- api/index.ts
+++ api/index.ts
@@ -6,7 +6,7 @@ import { handle } from "hono/vercel";
 const app = new Hono().basePath("/api");
 
 app.use("/*", cors({
-  origin: "https://website-iot-assignment-2.vercel.app/",
+  origin: "https://website-iot-assignment-2.vercel.app",
   allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 }));
 
